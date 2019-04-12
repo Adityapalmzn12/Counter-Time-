7fb97df0beecc44abab6ada84aca5d90052f157a
Adityapalmzn12 <adityapalmzn12@gmail.com>
2019-04-12 08:23:47 +0530

    Update Form1.cs

--- LTC TIME/Form1.cs
+++ LTC TIME/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace LTC_TIME
 {
@@ -15,6 +16,24 @@ namespace LTC_TIME
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+            string aditya = "LTC TIME : " + DateTime.Now.ToString("hh:mm:ss:fff");
+            string[] element = Regex.Split(aditya, ":");
+            int x = Convert.ToInt16(element[4]);
+            x = x / 40;
+             string ashu = "LTC TIME : " + DateTime.Now.ToString("hh:mm:ss");
+            label1.Text=  ashu +":" +x;
         }
     }
 }
